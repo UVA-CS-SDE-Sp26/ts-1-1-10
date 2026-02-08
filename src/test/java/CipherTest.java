0106ed5d47b6a3ab3ac90e6bfaad5fe9d01d8df0
Adam Ismail <adammismail3@gmail.com>
2026-02-08 16:09:57 -0500

    Update CipherTest assertions

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class CipherTest {
+
+/*
+What should I test?
+    - When there is no argument (should list the numbered files available to display)
+    - When it runs with a number as an argument (should display the contents of the file of that number)
+    - When it runs with a number that does not have a file to match (should provide an error)
+    - When it runs without a second parameter (should use the default key to decipher)
+    - When it runs with a second parameter (should supply an alternate key to use when deciphering)
+    - When it runs with a second parameter that is not a valid alternate key (should return error)
+
+*/
+
 }
